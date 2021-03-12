4eb20dd22f3d9d052ed8dd64906bf6c4f4cefbfd
Tomasz Walen <Tomasz.Walen@ing.com>
2021-03-12 16:02:01 +0100

    updated to bench script

--- bench.py
+++ bench.py
@@ -7,6 +7,7 @@ import pandas as pd
 from timeit import default_timer as timer
 import matplotlib.pyplot as plt
 import seaborn as sns
+import click
 
 class Timer(object):
 
@@ -48,28 +49,34 @@ def create_experiment(n_row=10**4, m_row=2*10**4, feat=100, density=0.15, n_top=
     return timings
 
 
-TIMINGS_CSV_FILENAME = "/tmp/timings.csv"
-TIMINGS_PNG_FILENAME = "/tmp/timings.png"
-n_row = 1*10**4
-m_row = 2*10**4
-feat = 500
-if os.path.exists(TIMINGS_CSV_FILENAME):
-    res = pd.read_csv(TIMINGS_CSV_FILENAME)
-else:
-    res = []
-    for density in np.arange(0.01, 0.201, 0.01):
-        curr = create_experiment(n_row=n_row, m_row=m_row, feat=feat, density=density)
-        curr['density'] = density
-        res.append(curr)
-    res = pd.DataFrame(res)
-    res.to_csv(TIMINGS_CSV_FILENAME, index=False)
+@click.command()
+@click.option("--n-row", type=int, default=10**4)
+@click.option("--m-row", type=int, default=2*10**4)
+@click.option("--feat", type=int, default=500)
+@click.option("--output-csv", type=click.Path(), default='/tmp/timings.csv')
+@click.option("--output-png", type=click.Path(), default='/tmp/timings.png')
+@click.option("--force", is_flag=True)
+def main(n_row, m_row, feat, output_csv, output_png, force):
+    if os.path.exists(output_csv) and force is False:
+        res = pd.read_csv(output_csv)
+    else:
+        res = []
+        for density in np.arange(0.01, 0.201, 0.01):
+            curr = create_experiment(n_row=n_row, m_row=m_row, feat=feat, density=density)
+            curr['density'] = density
+            res.append(curr)
+        res = pd.DataFrame(res)
+        res.to_csv(output_csv, index=False)
 
-sns.set()
-fig, ax = plt.subplots(figsize=(10, 5))
-res.plot(x='density', y=['dense', 'sparse'], ax=ax)
-ax.legend()
-ax.set_ylabel("time (sec.)")
-ax.set_title(f"Sparse vs Dense Dot Topn (names_to_match={n_row} gt={m_row} feat={feat})")
-sns.despine(ax=ax)
-plt.savefig(TIMINGS_PNG_FILENAME, dpi=100)
-plt.show()
+    sns.set()
+    _, ax = plt.subplots(figsize=(10, 5))
+    res.plot(x='density', y=['dense', 'sparse'], ax=ax)
+    ax.legend()
+    ax.set_ylabel("time (sec.)")
+    ax.set_title(f"Sparse vs Dense Dot Topn (names_to_match={n_row} gt={m_row} feat={feat})")
+    sns.despine(ax=ax)
+    plt.savefig(output_png, dpi=100)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
